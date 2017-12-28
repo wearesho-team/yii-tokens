4980299f21e0fe1be12e704a2ff3b8a7cc2d8228
Alexander Letnikow <reclamme@gmail.com>
2017-12-28 14:05:52 +0200

    Fix TokenRepositorySettingsTest

--- tests/Configs/TokenRepositorySettingsTest.php
+++ tests/Configs/TokenRepositorySettingsTest.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Wearesho\Yii\Tests\Configs;
-
 
 use Carbon\CarbonInterval;
 
@@ -12,36 +10,59 @@ use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 use Wearesho\Yii\Configs\TokenRepositoryConfig;
 
+/**
+ * Class TokenRepositorySettingsTest
+ * @package Wearesho\Yii\Tests\Configs
+ */
 class TokenRepositorySettingsTest extends TestCase
 {
-    public function testInstantiatingException()
+    /** @var TokenRepositoryConfig */
+    protected $config;
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     * @throws \yii\di\NotInstantiableException
+     */
+    protected function setUp()
     {
-        $config = [];
-        $this->expectException(InvalidConfigurationException::class);
-        TokenRepositoryConfig::instantiate($config);
+        parent::setUp();
+        $this->config = \Yii::$container->get(TokenRepositoryConfig::class);
     }
 
-    public function testInstantiating()
+    public function testExpire()
     {
-        $config = [
-            TokenRepositoryConfig::CONFIG_ROOT => [
-                'expirePeriod' => $expirePeriodMinutes = 20,
-                'deliveryLimit' => $deliveryLimit = 2,
-                'verifyLimit' => $verifyLimit = 4,
-            ],
-        ];
-        $instance = TokenRepositoryConfig::instantiate($config);
-        $this->assertEquals(
-            CarbonInterval::minutes($expirePeriodMinutes),
-            $instance->getExpirePeriod()
-        );
-        $this->assertEquals(
-            $deliveryLimit,
-            $instance->getDeliveryLimit()
-        );
-        $this->assertEquals(
-            $verifyLimit,
-            $instance->getVerifyLimit()
-        );
+        putenv("{$this->config->expirePeriodKey}=0");
+
+        $this->config->defaultExpirePeriod = $expirePeriod = mt_rand(1, 10);
+        $this->assertEquals($expirePeriod, $this->config->getExpirePeriod()->minutes);
+
+        $this->config->defaultExpirePeriod = 0;
+        putenv("{$this->config->expirePeriodKey}={$expirePeriod}");
+        $this->assertEquals($expirePeriod, $this->config->getExpirePeriod()->minutes);
     }
+
+    public function testDeliveryLimit()
+    {
+        putenv("{$this->config->deliveryLimitKey}=0");
+
+        $this->config->defaultDeliveryLimit = $deliveryLimit = mt_rand(1, 10);
+        $this->assertEquals($deliveryLimit, $this->config->getDeliveryLimit());
+
+        $this->config->defaultDeliveryLimit = 0;
+        putenv("{$this->config->deliveryLimitKey}={$deliveryLimit}");
+        $this->assertEquals($deliveryLimit, $this->config->getDeliveryLimit());
+    }
+
+    public function testVerifyLimit()
+    {
+        putenv("{$this->config->verifyLimitKey}=0");
+
+        $this->config->defaultVerifyLimit = $verifyLimit = mt_rand(1, 10);
+        $this->assertEquals($verifyLimit, $this->config->getVerifyLimit());
+
+        $this->config->defaultVerifyLimit = 0;
+        putenv("{$this->config->verifyLimitKey}={$verifyLimit}");
+        $this->assertEquals($verifyLimit, $this->config->getVerifyLimit());
+    }
+
 }
