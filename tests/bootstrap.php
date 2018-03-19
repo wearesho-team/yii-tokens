05a8134ce8ad51b3b3706ad535718f132659120b
Alexander Letnikow <reclamme@gmail.com>
2018-03-19 17:21:50 +0200

    Fix CI bug with missing .env file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,9 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
 $env = new Dotenv(dirname(__DIR__));
-$env->load();
+
+if(file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env')) {
+    $env->load();
+}
 
 $env->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_TYPE',]);
